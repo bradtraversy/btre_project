4fca7448b4820c74732f9ed9d5ad7953381722c4
Brad Traversy <traversymedia@gmail.com>
2018-09-21 13:08:24 -0400

    Added pages app

--- btre/settings.py
+++ btre/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
