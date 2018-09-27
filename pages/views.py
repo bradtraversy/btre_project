e9a09024d96ffb425c9bc6d72a42be22fdbf1d86
Brad Traversy <traversymedia@gmail.com>
2018-09-27 18:47:45 -0400

    Dynamic home & about data display

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+from realtors.models import Realtor
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings
+    }
+
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    # Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+
+    return render(request, 'pages/about.html', context)
