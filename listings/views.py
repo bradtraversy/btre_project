bd1caf4c1266be2392ac0f0e7d4dc68910396b05
Brad Traversy <traversymedia@gmail.com>
2018-09-28 12:36:32 -0400

    Search form filtering

--- listings/views.py
+++ listings/views.py
@@ -27,11 +27,43 @@ def listing(request, listing_id):
   return render(request, 'listings/listing.html', context)
 
 def search(request):
+  queryset_list = Listing.objects.order_by('-list_date')
+
+  # Keywords
+  if 'keywords' in request.GET:
+    keywords = request.GET['keywords']
+    if keywords:
+      queryset_list = queryset_list.filter(description__icontains=keywords)
+
+  # City
+  if 'city' in request.GET:
+    city = request.GET['city']
+    if city:
+      queryset_list = queryset_list.filter(city__iexact=city)
+
+  # State
+  if 'state' in request.GET:
+    state = request.GET['state']
+    if state:
+      queryset_list = queryset_list.filter(state__iexact=state)
+
+  # Bedrooms
+  if 'bedrooms' in request.GET:
+    bedrooms = request.GET['bedrooms']
+    if bedrooms:
+      queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+  # Price
+  if 'price' in request.GET:
+    price = request.GET['price']
+    if price:
+      queryset_list = queryset_list.filter(price__lte=price)
 
   context = {
     'state_choices': state_choices,
     'bedroom_choices': bedroom_choices,
-    'price_choices': price_choices
+    'price_choices': price_choices,
+    'listings': queryset_list
   }
 
   return render(request, 'listings/search.html', context)
