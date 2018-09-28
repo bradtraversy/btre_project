b45d881029109f6cec3358ca419349201c0ef3da
Brad Traversy <traversymedia@gmail.com>
2018-09-28 12:13:04 -0400

    Search form choices

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, state_choices
 
 from .models import Listing
 
@@ -26,4 +27,11 @@ def listing(request, listing_id):
   return render(request, 'listings/listing.html', context)
 
 def search(request):
-  return render(request, 'listings/search.html')
+
+  context = {
+    'state_choices': state_choices,
+    'bedroom_choices': bedroom_choices,
+    'price_choices': price_choices
+  }
+
+  return render(request, 'listings/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 from listings.models import Listing
 from realtors.models import Realtor
@@ -8,7 +9,10 @@ def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     context = {
-        'listings': listings
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
     }
 
     return render(request, 'pages/index.html', context)
