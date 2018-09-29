f97656b7d9d7ab16e82a6384809316dc523fb907
Brad Traversy <traversymedia@gmail.com>
2018-09-29 19:07:02 -0400

    User registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 def register(request):
   if request.method == 'POST':
-    messages.error(request, 'Testing error message')
-    return redirect('register')
+    # Get form values
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+
+    # Check if passwords match
+    if password == password2:
+      # Check username
+      if User.objects.filter(username=username).exists():
+        messages.error(request, 'That username is taken')
+        return redirect('register')
+      else:
+        if User.objects.filter(email=email).exists():
+          messages.error(request, 'That email is being used')
+          return redirect('register')
+        else:
+          # Looks good
+          user = User.objects.create_user(username=username, password=password,email=email, first_name=first_name, last_name=last_name)
+          # Login after register
+          # auth.login(request, user)
+          # messages.success(request, 'You are now logged in')
+          # return redirect('index')
+          user.save()
+          messages.success(request, 'You are now registered and can log in')
+          return redirect('login')
+    else:
+      messages.error(request, 'Passwords do not match')
+      return redirect('register')
   else:
     return render(request, 'accounts/register.html')
 
