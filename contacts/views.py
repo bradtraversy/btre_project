9c161708d35c9d75d073b0e5c4f014c1045f6d94
Brad Traversy <traversymedia@gmail.com>
2018-09-30 07:41:33 -0400

    Contact form submission

--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Contact
 
 def contact(request):
-  return
+  if request.method == 'POST':
+    listing_id = request.POST['listing_id']
+    listing = request.POST['listing']
+    name = request.POST['name']
+    email = request.POST['email']
+    phone = request.POST['phone']
+    message = request.POST['message']
+    user_id = request.POST['user_id']
+    realtor_email = request.POST['realtor_email']
+
+    contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id )
+
+    contact.save()
+
+    messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
+    return redirect('/listings/'+listing_id)
+
