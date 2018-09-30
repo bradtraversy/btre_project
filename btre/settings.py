894ddb0655315944e0d23d77648c4d30ada419d5
Brad Traversy <traversymedia@gmail.com>
2018-09-29 21:10:01 -0400

    Contacts app & model

--- btre/settings.py
+++ btre/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ contacts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from datetime import datetime
+
+class Contact(models.Model):
+  listing = models.CharField(max_length=200)
+  listing_id = models.IntegerField()
+  name = models.CharField(max_length=200)
+  email = models.CharField(max_length=100)
+  phone = models.CharField(max_length=100)
+  message = models.TextField(blank=True)
+  contact_date = models.DateTimeField(default=datetime.now, blank=True)
+  user_id = models.IntegerField(blank=True)
+  def __str__(self):
+    return self.name
