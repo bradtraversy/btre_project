e4ce3f093efe325fa0f62b7f2ccda593169f7a6e
Brad Traversy <traversymedia@gmail.com>
2018-09-29 21:36:29 -0400

    Contact form prep

--- btre/urls.py
+++ btre/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('listings/', include('listings.urls')),
     path('accounts/', include('accounts.urls')),
+    path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def contact(request):
+  return
