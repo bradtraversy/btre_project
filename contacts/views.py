d3f287fd20ae495fe846ea05d1239519aaa17d28
Brad Traversy <traversymedia@gmail.com>
2018-10-01 17:59:52 -0400

    Disable email

--- contacts/views.py
+++ contacts/views.py
@@ -27,13 +27,13 @@ def contact(request):
     contact.save()
 
     # Send email
-    send_mail(
-      'Property Listing Inquiry',
-      'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
-      'traversy.brad@gmail.com',
-      [realtor_email, 'techguyinfo@gmail.com'],
-      fail_silently=False
-    )
+    # send_mail(
+    #   'Property Listing Inquiry',
+    #   'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
+    #   'traversy.brad@gmail.com',
+    #   [realtor_email, 'techguyinfo@gmail.com'],
+    #   fail_silently=False
+    # )
 
     messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
     return redirect('/listings/'+listing_id)
