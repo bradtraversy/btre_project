c64c2a09e1e70680e3c869083fe968d62f807522
Brad Traversy <traversymedia@gmail.com>
2018-10-01 11:01:19 -0400

    Removed email from settings

--- btre/settings.py
+++ btre/settings.py
@@ -142,13 +142,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER='traversy.brad@gmail.com'
-EMAIL_HOST_PASSWORD=''
-EMAIL_USE_TLS=True
-
 try:
     from .local_settings import *
 except ImportError:
